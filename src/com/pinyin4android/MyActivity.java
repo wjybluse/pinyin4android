72ec03fd70cd9dbd9a0095d2d9770cbd24a4e785
Ryanch741 <Ryanch741@gmail.com>
2011-05-30 14:51:36 +0000

    add comment.

--- src/com/pinyin4android/MyActivity.java
+++ src/com/pinyin4android/MyActivity.java
@@ -1,9 +1,33 @@
+/**
+ * Copyright (C) 2011 Pinyin for Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.pinyin4android;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+/**
+ * this is a demo activity for pinyin4android
+ * you can running this activity in your mobile to check it .
+ *
+ * User: Ryan
+ * Date: 11-5-29
+ * Time: 21:13
+ */
 public class MyActivity extends Activity {
     /**
      * Called when the activity is first created.
