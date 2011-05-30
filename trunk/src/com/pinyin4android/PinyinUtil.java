51d6e7d66dbe53edfdb3820f43e3ef5ea1144e72
Ryanch741@gmail.com <Ryanch741@gmail.com@9c770ac3-5b24-0828-7e83-37e7c997f4ee>
2011-05-30 14:51:36 +0000

    add comment.

--- trunk/src/com/pinyin4android/PinyinUtil.java
+++ trunk/src/com/pinyin4android/PinyinUtil.java
@@ -1,3 +1,19 @@
+/**
+ * Copyright (C) 2011 Pinyin for Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.pinyin4android;
 
 import android.content.Context;
@@ -7,13 +23,24 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 
 /**
+ * This class is pinyin4android main interface .
+ * there are two methods you can call them to convert the chinese to pinyin.
+ * PinyinUtil.toPinyin(Context context,char c);
+ * PinyinUtil.toPinyin(Context context,String hanzi);
+ * <p/>
  * User: Ryan
  * Date: 11-5-29
- * Time: 下午9:13
+ * Time: 21:13
  */
 public abstract class PinyinUtil {
-
-    public static String toPinyin(Context context,char c) {
+    /**
+     * to convert chinese to pinyin
+     *
+     * @param context Android Context
+     * @param c       the chinese character
+     * @return pinyin
+     */
+    public static String toPinyin(Context context, char c) {
         if (c >= 'A' && c <= 'Z') {
             return String.valueOf((char) (c + 32));
         }
@@ -46,7 +73,14 @@ public abstract class PinyinUtil {
         return null;
     }
 
-    public static String toPinyin(Context context,String hanzi) {
+    /**
+     * to convert chinese to pinyin
+     *
+     * @param context Android Context
+     * @param hanzi   the chinese string
+     * @return pinyin
+     */
+    public static String toPinyin(Context context, String hanzi) {
         StringBuffer sb = new StringBuffer("");
         RandomAccessFile is = null;
         try {
